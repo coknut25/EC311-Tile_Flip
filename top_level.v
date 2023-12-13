3a8f1425937ca1054a0948acab2d7d44d7b3bdab
ajoshua2004 <113662656+ajoshua2004@users.noreply.github.com>
2023-12-13 15:53:56 -0500

    Update top_level.v

--- top_level.v
+++ top_level.v
@@ -1,121 +1,88 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/09/2023 03:37:46 PM
-// Design Name: 
-// Module Name: top_level
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-module top_level(
-    input clk,  // System clock at 50 MHz
-    input reset,  // System reset
-    input [15:0] switches,  // Input switches
-    input button,
-    output [15:0] leds,  // Output LEDs
-    // VGA outputs (if using VGA display)
-    output hsync,
-    output vsync,
-    output [11:0] rgb
+// This module represents the main controller for the game system.
+// It handles the integration of various modules such as the clock divider, debouncer for buttons, home screen,
+// level selection, game logic, VGA driver, and score counter.
+
+module main(
+    input clk,        // System clock
+    input reset,      // System reset
+    input [15:0] switches,   // Input switches for game interaction
+    input [3:0] level_buttons,   // Buttons for level selection
+    output [15:0] leds,   // Output LEDs for game feedback
+    output [3:0] VGA_R,   // VGA Red channel
+    output [3:0] VGA_G,   // VGA Green channel
+    output [3:0] VGA_B,   // VGA Blue channel
+    output VGA_HS,   // VGA Horizontal Sync
+    output VGA_VS,   // VGA Vertical Sync
+    output wire [6:0] seg,   // Output from the score counter module
+    output wire [3:0] an   // Output from the score counter module
 );
 
-    // Internal signals for interconnecting submodules
-    wire [15:0] debounced_switches;
-    wire [47:0] shuffled_array;
-    wire [2:0] random_number;
-    wire [15:0] game_state, matched_tiles, mismatched_tiles;
-    reg start_shuffle_signal = 0;
-    reg btn_start_prev = 0;
+    // Wires for interconnecting modules
+    wire [47:0] tile_values;
+    wire [1:0] current_level;
+    wire game_start;
+    wire clk_divided;
+    wire [15:0] dswitches, dbuttons;
 
-    wire clk_25MHz;  // 25 MHz clock for VGA
-
-    // Instantiate the clock divider
-    clk_divider divider(
+    // Clock divider instantiation
+    clk_divider clk_div (
         .in_clk(clk),
-        .out_clk(clk_25MHz)
-    );
-   
-    always@(posedge clk) begin
-        if(reset) begin
-            start_shuffle_signal <= 0;
-            btn_start_prev <= 0;
-        end else begin
-            if(button && !btn_start_prev) begin
-                start_shuffle_signal <= 1;
-            end else begin
-                start_shuffle_signal <= 0;
-            end
-           
-            btn_start_prev <= button;
-        end
-    end
-    // Instantiate the debouncer
-    debouncer switch (
-        .clk(clk),
-        .reset(reset),
-        .noisy_switch(switches),
-        .clean_switch(debounced_switches)
-    );
-    
-    wire debounced_button; 
-    
-    debouncer btn (
-        .clk(clk),
-        .reset(reset),
-        .noisy_switch(button),
-        .clean_switch(debounced_button)
-        );
-
-    // Instantiate the LFSR
-    lfsr_random lfsr (
-        .clk(clk),
-        .reset(reset),
-        .random_num(random_number)
+        .clk(clk_divided)
     );
 
-    // Instantiate the array shuffle
-    array_shuffle shuffle (
+    // Debouncer for level buttons instantiation
+    debouncer_btn dbutton (
         .clk(clk),
-        .reset(reset),
-        .start(start_shuffle_signal),  // You need to define how and when to start shuffling
-        .array(shuffled_array)
+        .noise(level_buttons),
+        .calm(dbuttons)
     );
 
-    // Instantiate the game logic
-    game_logic gl (
-    .clk(clk),
-    .reset(reset),
-    .switches(debounced_switches), // Connect debounced switches
-    .tile_states(), // Optionally connect if you need to monitor tile states externally
-    .tile_values_flat(), // Optionally connect if needed externally
-    .game_reset(), // Optionally connect if needed externally
-    .tries() // Optionally connect if needed externally
-);
+    // Home screen module instantiation
+    home_screen home (
+        .clk(clk_divided),
+        .reset(reset),
+        .level_buttons(dbuttons),
+        .level(current_level),
+        .start(game_start)
+    );
 
-    // Instantiate the VGA driver (if using VGA display)
+    // Level selection module instantiation
+    level_select level_sel (
+        .clk(clk),
+        .reset(reset),
+        .level(current_level),
+        .tile_setup(tile_values)
+    );
+
+    // Game logic module instantiation
+    game_logic game (
+        .clk(clk),
+        .reset(reset),
+        .switches(switches),
+        .leds(leds),
+        .tiles(tile_values)
+    );
+
+    // VGA driver module instantiation
     vga_driver vga (
-        .clk(clk_25MHz),
-        .reset(reset),
-        .game_state(game_state),
-        .matched_tiles(matched_tiles),
-        .mismatched_tiles(mismatched_tiles),
-        .hsync(hsync),
-        .vsync(vsync),
-        .rgb(rgb)
+        .in_clk(clk),
+        .states(leds), 
+        .tile_vals(tile_values),
+        .VGA_R(VGA_R),
+        .VGA_G(VGA_G),
+        .VGA_B(VGA_B),
+        .VGA_HS(VGA_HS),
+        .VGA_VS(VGA_VS)
     );
 
-    
+    // Score counter module instantiation
+    score_counter score (
+        .clock_50Mhz(clk),
+        .reset(reset),
+        .Anode_Activate(an),
+        .LED_out(seg)
+    );
 
 endmodule
